b7ba67fa5671a384752a39151e1aa502300dfc14
mehulmorker <mehul.morker98@gmail.com>
2025-08-06 13:30:56 +0530

    added Leftsidebar and chat in chat page

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { LeftSidebar } from "./LeftSidebar";
+import { MainChatArea } from "./MainChatArea";
 
 export const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="flex min-h-full">
+      <LeftSidebar />
+      <MainChatArea />
+    </div>
+  );
+};
